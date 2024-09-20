9bfe128c3fe32a658117aae7add89ee17c2db517
SanaShaikh2191 <sanashaikh.4448gmail.com>
2024-09-20 22:04:17 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/utils/text.dart';
+import 'package:movie_app/widgets/toprated.dart';
+import 'package:movie_app/widgets/trending.dart';
+import 'package:movie_app/widgets/tv.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,16 +12,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        
-        brightness: Brightness.dark
-      ),
+      theme: ThemeData(brightness: Brightness.dark),
       home: const Home(),
     );
   }
@@ -30,9 +30,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List trendingmovies = [];
+  List topratedmovies = [];
+  List tv = [];
+  final String apikey = 'dab3e0287852be3d6170e13579e8e4ec';
+  final readaccesstoken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYWIzZTAyODc4NTJiZTNkNjE3MGUxMzU3OWU4ZTRlYyIsIm5iZiI6MTcyNjgyNTI5Ny44MzEwNjcsInN1YiI6IjY2ZWIwMTJiYjI5MTdlYjE4MDBiYzBkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lXfLm980klagmKQAz3DX8XRrATo4xxt0GKlztHJnsOE';
+
+  @override
+  void initState() {
+    loadmovies();
+    super.initState();
+  }
+
+  loadmovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apikey, readaccesstoken),
+        logConfig: ConfigLogger(showLogs: true, showErrorLogs: true));
+    Map treandingresult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topratedresult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvresult = await tmdbWithCustomLogs.v3.tv.getPopular();
+
+    setState(() {
+      trendingmovies = treandingresult['results'];
+      topratedmovies = topratedresult['results'];
+      tv = tvresult['results'];
+    });
+    print(trendingmovies);
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Flutter Movie App'),),);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: ModifiedText(
+          text: 'Flutter Movie App❤️',
+          color: Colors.white,
+          size: 20,
+        ),
+      ),
+      body: ListView(
+        children: [
+          TV(tv: tv),
+          TopRatedMovies(toprated: topratedmovies),
+          TrendingMovies(trending: trendingmovies),
+        ],
+      ),
+    );
   }
 }
-
